f107c0f7b413228b4d23bd39e1c50badfa3454ac
PavloPD <devmapps@gmail.com>
2020-07-26 22:04:59 +0300

    task1

--- src/test/java/TodoMvcTestXpath/TodoMvcTask.java
+++ src/test/java/TodoMvcTestXpath/TodoMvcTask.java
@@ -1,4 +1,4 @@
-package com.taotas.selenideIntro;
+package TodoMvcTestXpath;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
@@ -8,29 +8,23 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class TodoMvcTask2 {
+public class TodoMvcTask {
+
     @Test
     void completedTask(){
 
-        // open TodoMVC page
         open("http://todomvc.com/examples/emberjs/");
 
-        // add tasks: "a", "b", "c"
         element(byXpath("//*[@id='new-todo']")).setValue("a").pressEnter();
         element(byXpath("//*[@id='new-todo']")).setValue("b").pressEnter();
         element(byXpath("//*[@id='new-todo']")).setValue("c").pressEnter();
-        // tasks should be "a", "b", "c"
 
         elements(byXpath("//*[@id='todo-list']/li")).shouldHave(exactTexts("a","b","c"));
 
-        // toggle b
-
         element(byXpath("//*[@id='todo-list']/li[2]//*[contains(concat(' ',normalize-space(@class),' '),' toggle ')]")).click();
 
-        // completed tasks should be b
-
         elements(byXpath("//*[@id='todo-list']/li[contains(concat(' ',normalize-space(@class),' '),' completed ')]")).shouldHave(exactTexts("b"));
-        // active tasks should be a, c
+
         element(byXpath("//*[@id='todo-list']/li[2]")).shouldNotHave(cssClass(".completed"));
         element(byXpath("//*[@id='todo-list']/li[2]")).shouldNotHave(cssClass(".completed"));
 
